b8626efed749b260b45295162c71820cb5648379
EricWARodrigues <128868563+EricWARodrigues@users.noreply.github.com>
2024-01-21 12:59:16 -0300

    Update gerador_senha.py
    Adicionado menu de interação
    
    A primeira letra da senha é a primeira letra do nome informado

--- gerador_senha.py
+++ gerador_senha.py
@@ -9,10 +9,17 @@ mapa_caracteres = letra_min + letra_mai + numeros + especiais
 
 senha = ''
 
-def gerarsenha(senha):
+def gerarsenha(letra, senha):
+    senha += letra
     for i in range(9):
         caractere = choice(mapa_caracteres)
         senha += caractere
     return senha
 
-print(gerarsenha(senha))
+print('-' * 30)
+print('Bem vindo ao gerador de SENHA')
+print('-' * 30)
+
+nome = input('\nInforme seu nome: ')
+primeira_letra = nome[0]
+print(f'\nGeramos a senha "{gerarsenha(primeira_letra, senha)}"')
